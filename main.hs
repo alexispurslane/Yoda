e7948f71723d333f5daa396d28abe5c1f2fdc5e0
christopherdumas <christopherdumas@me.com>
2015-08-22 14:58:42 -0700

    almost finished interpreter

--- main.hs
+++ main.hs
@@ -1,22 +1,64 @@
-module Main where
+ module Main where
 
-import qualified Data.Text as T
-import Data.Char
+ import qualified Data.Text as T
+ import Data.Char
 
-data YodaVal = Number Int
-             | Str String
-             | Id String
-             | Decimal Float
-             | Error String
-             deriving (Show)
+ import qualified Data.Map as Map
 
-allMatch f str = all (==True) (map f str)
+ data YodaVal = Number Int
+              | Str String
+              | Id String
+              | Decimal Float
+              | Error String
+              deriving (Show)
 
-strToVal :: String -> YodaVal
-strToVal str
-  | allMatch (not . isDigit) str = Id str
-  | allMatch isDigit str = Number $ read str
-  | otherwise = Error "Unknown form."
+ defaultEnv = Map.fromList [("+", numericBinop (+)),
+                            ("-", numericBinop  (-)),
+                            ("*", numericBinop (*)),
+                            ("/", numericBinop div)]
 
-parse :: String -> [YodaVal]
-parse str = map (strToVal . T.unpack) (T.splitOn (T.pack " ") (T.pack str))
+ numericBinop :: (Int -> Int -> Int) -> [YodaVal] -> YodaVal
+ numericBinop op params = (Number . foldl1 op) $ map unpackNumber params
+
+ unpackNumber :: YodaVal -> Int
+ unpackNumber x = case x of
+   Number v -> v
+   otherwise -> 0
+
+ unpackString :: YodaVal -> String
+ unpackString x = case x of
+   Str v -> v
+   otherwise -> ""
+
+ unpackDecimal :: YodaVal -> Float
+ unpackDecimal x = case x of
+   Decimal v -> v
+   Number v -> fromIntegral v
+   otherwise -> 0.0
+
+ allMatch f str = all (==True) (map f str)
+
+ strToVal :: String -> YodaVal
+ strToVal str
+   | allMatch (not . isDigit) str = Id str
+   | allMatch isDigit str = Number $ read str
+   | otherwise = Error "Unknown form."
+
+ parse :: String -> [YodaVal]
+ parse str = map (strToVal . T.unpack) (T.splitOn (T.pack " ") (T.pack str))
+
+ push :: a -> [a] -> [a]
+ push x xs = xs ++ [x]
+
+ pop :: [a] -> (a, [a])
+ pop xs = (last xs, init xs)
+
+ run :: [YodaVal] -> [YodaVal] -> Map.Map String ([YodaVal] -> YodaVal) -> [YodaVal]
+ run exps stack env = foldr (eval env) stack exps
+                      where eval env e s = case e of
+                              v@(Number _)  -> v : s
+                              v@(Str _)     -> v : s
+                              v@(Decimal _) -> v : s
+                              v@(Error _)   -> [v]
+                              (Id v)        -> exec s (env Map.! v) 
+                            exec s f = [Str "Not implemented. Sucking all arguments"]
