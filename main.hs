33cb495a04d233821b45963511f81f3b88a1d468
christopherdumas <christopherdumas@me.com>
2015-08-22 20:15:12 -0700

    Revised and more well-thought out design for running. Basic things done.

--- main.hs
+++ main.hs
@@ -1,64 +1,73 @@
- module Main where
+module Main where
 
- import qualified Data.Text as T
- import Data.Char
+import qualified Data.Text as T
+import Data.Char
 
- import qualified Data.Map as Map
+import qualified Data.Map as Map
 
- data YodaVal = Number Int
-              | Str String
-              | Id String
-              | Decimal Float
-              | Error String
-              deriving (Show)
+data YodaVal = Number Int
+             | Str String
+             | Id String
+             | Decimal Float
+             | Error String
+             deriving (Show)
 
- defaultEnv = Map.fromList [("+", numericBinop (+)),
-                            ("-", numericBinop  (-)),
-                            ("*", numericBinop (*)),
-                            ("/", numericBinop div)]
+defaultEnv :: Map.Map String (YodaVal -> YodaVal -> YodaVal, Int)
+defaultEnv = Map.fromList [("+", (numericBinop (+), 2)),
+                           ("-", (numericBinop (-), 2)),
+                           ("*", (numericBinop (*), 2)),
+                           ("/", (numericBinop div, 2))]
 
- numericBinop :: (Int -> Int -> Int) -> [YodaVal] -> YodaVal
- numericBinop op params = (Number . foldl1 op) $ map unpackNumber params
+numericBinop :: (Int -> Int -> Int) -> YodaVal -> YodaVal -> YodaVal
+numericBinop op x y = Number (unpackNumber x `op` unpackNumber y)
 
- unpackNumber :: YodaVal -> Int
- unpackNumber x = case x of
-   Number v -> v
-   otherwise -> 0
+unpackNumber :: YodaVal -> Int
+unpackNumber x = case x of
+  Number v -> v
+  otherwise -> 0
 
- unpackString :: YodaVal -> String
- unpackString x = case x of
-   Str v -> v
-   otherwise -> ""
+unpackString :: YodaVal -> String
+unpackString x = case x of
+  Str v -> v
+  otherwise -> ""
 
- unpackDecimal :: YodaVal -> Float
- unpackDecimal x = case x of
-   Decimal v -> v
-   Number v -> fromIntegral v
-   otherwise -> 0.0
+unpackDecimal :: YodaVal -> Float
+unpackDecimal x = case x of
+  Decimal v -> v
+  Number v -> fromIntegral v
+  otherwise -> 0.0
 
- allMatch f str = all (==True) (map f str)
+allMatch f str = all (==True) (map f str)
 
- strToVal :: String -> YodaVal
- strToVal str
-   | allMatch (not . isDigit) str = Id str
-   | allMatch isDigit str = Number $ read str
-   | otherwise = Error "Unknown form."
+strToVal :: String -> YodaVal
+strToVal str
+  | allMatch (not . isDigit) str = Id str
+  | allMatch isDigit str = Number $ read str
+  | otherwise = Error "Unknown form."
 
- parse :: String -> [YodaVal]
- parse str = map (strToVal . T.unpack) (T.splitOn (T.pack " ") (T.pack str))
+parse :: String -> [YodaVal]
+parse str = map (strToVal . T.unpack) (T.splitOn (T.pack " ") (T.pack str))
 
- push :: a -> [a] -> [a]
- push x xs = xs ++ [x]
+push :: a -> [a] -> [a]
+push x xs = xs ++ [x]
 
- pop :: [a] -> (a, [a])
- pop xs = (last xs, init xs)
+pop :: [a] -> (a, [a])
+pop xs = (last xs, init xs)
 
- run :: [YodaVal] -> [YodaVal] -> Map.Map String ([YodaVal] -> YodaVal) -> [YodaVal]
- run exps stack env = foldr (eval env) stack exps
-                      where eval env e s = case e of
-                              v@(Number _)  -> v : s
-                              v@(Str _)     -> v : s
-                              v@(Decimal _) -> v : s
-                              v@(Error _)   -> [v]
-                              (Id v)        -> exec s (env Map.! v) 
-                            exec s f = [Str "Not implemented. Sucking all arguments"]
+eval env e s = case e of
+  v@(Number _)  -> v : s
+  v@(Str _)     -> v : s
+  v@(Decimal _) -> v : s
+  (Id v)        -> exec s v (env Map.! v)
+  otherwise     -> [Error "Unknown form or expression."]
+  where
+    exec s i f = case f of
+      (fn, n)   -> [Str $ "Not implemented. Function " ++ i ++ " of " ++ show n ++ " arguments."]
+
+
+run :: [YodaVal] -> [YodaVal] -> Map.Map String (YodaVal -> YodaVal -> YodaVal, Int) -> [YodaVal]
+run [] s _ = s
+run exps stack env = let r = eval env (head exps) stack
+                     in case r of
+                       v@[Error _] -> v
+                       otherwise -> run (tail exps) r env
