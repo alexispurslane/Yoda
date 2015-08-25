91fa35fb3e8db07d4203c5a68d65ac8b1804f699
christopherdumas <christopherdumas@me.com>
2015-08-25 12:40:36 -0700

    Lint and clean

--- repl.hs
+++ repl.hs
@@ -13,10 +13,10 @@ repl s = do
   putStr ") "
   hFlush stdout -- Make sure the prompt shows first
   maybeLine <- getLine
-  putStrLn (show (Map.keys (snd s)))
+  print (Map.keys (snd s))
   case maybeLine of
    "exit" -> return ()
-   line -> do let ans = eval line (fst s) (snd s)
+   line -> do let ans = uncurry (eval line) s
               putStrLn $ "    " ++ case ans of
                 ([Error e], _) -> "ERROR: " ++ e
                 (v, _)     -> intercalate "\n    " (map show v)
