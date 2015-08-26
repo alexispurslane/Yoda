4a870f983f1e8bb6d0cfe956204dcd8bfb429eee
christopherdumas <christopherdumas@me.com>
2015-08-26 15:38:35 -0700

    Improved repl

--- repl.hs
+++ repl.hs
@@ -5,20 +5,22 @@ import Parser
 import Interpreter
 
 import System.IO
+import System.Console.Readline
 
 import Data.List
 import qualified Data.Map as Map
 
 repl s = do
-  putStr ") "
-  hFlush stdout -- Make sure the prompt shows first
-  maybeLine <- getLine
+  maybeLine <- readline ":-) "
   case maybeLine of
-   "exit" -> return ()
-   line -> do let ans = uncurry (eval line) s
-              putStrLn $ "    " ++ case ans of
-                ([Error e], _) -> "ERROR: " ++ e
-                (v, _)     -> intercalate "\n    " (map show v)
-              repl ans
+    Nothing      -> return ()
+    Just "exit"  -> return ()
+    Just "quit"  -> return ()
+    Just line    -> do addHistory line
+                       let ans = uncurry (eval line) s
+                       putStrLn $ "    " ++ case ans of
+                         ([Error e], _) -> "ERROR: " ++ e
+                         (v, _)     -> intercalate "\n    " (map show v)
+                       repl ans
 
 main = repl ([], defaultEnv)
